0124761eee66163c9afa64a13d59972d27277b8f
MAPLELEAF3659 <ig910425@gmail.com>
2023-05-30 14:27:36 +0800

    Fix turn backward

--- Assets/Scripts/SnakeController.cs
+++ Assets/Scripts/SnakeController.cs
@@ -3,6 +3,14 @@ using System.Collections;
 using Unity.Collections;
 using UnityEngine;
 
+enum Direction
+{
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
 public class SnakeController : MonoBehaviour
 {
     GameController gameController;
@@ -13,6 +21,8 @@ public class SnakeController : MonoBehaviour
     bool isHitObstacle;
     int length = 1;
 
+    Direction direction = Direction.Up;
+
     void Start()
     {
         gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
@@ -21,21 +31,25 @@ public class SnakeController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && !direction.Equals(Direction.Down))
         {
             transform.rotation = Quaternion.Euler(0, 0, 0);
+            direction = Direction.Up;
         }
-        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow) && !direction.Equals(Direction.Left))
         {
             transform.rotation = Quaternion.Euler(0, 90, 0);
+            direction = Direction.Right;
         }
-        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) && !direction.Equals(Direction.Up))
         {
             transform.rotation = Quaternion.Euler(0, 180, 0);
+            direction = Direction.Down;
         }
-        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow) && !direction.Equals(Direction.Right))
         {
             transform.rotation = Quaternion.Euler(0, 270, 0);
+            direction = Direction.Left;
         }
     }
 
